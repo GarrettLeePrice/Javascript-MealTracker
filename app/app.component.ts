02f8d50219c146000971b1cf1cae7f7e42437888
Garrett Price <garrettleeprice@gmail.com>
2016-10-21 10:17:50 -0700

    add done editing button to make edit form disappear

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,20 +4,26 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <div class="container">
-      <h1>Meal Tracker</h1>
-      <h3 (click)="showDetails(currentMeal)" *ngFor="let currentMeal of meals">{{ currentMeal.name }}</h3>
+    <h1>Meal Tracker</h1>
+    <div *ngFor="let currentMeal of meals">
+      <h3>{{ currentMeal.name }}</h3>
+      <button (click)="showDetails(currentMeal)">Edit</button>
+    </div>
+    <div *ngIf="selectedMeal">
       <h1>Edit Meal</h1>
-    <div>
-    <label>Enter Meal Name:</label>
-    <input [(ngModel)]="selectedMeal.name">
-    </div>
-    <div>
-      <label>Enter Meal Description:</label>
-      <input [(ngModel)]="selectedMeal.description">
-    </div>
-    <div>
-    <label>Enter Calories:</label>
-    <input [(ngModel)]="selectedMeal.calories">
+      <div>
+        <label>Enter Meal Name:</label>
+        <input [(ngModel)]="selectedMeal.name">
+      </div>
+      <div>
+        <label>Enter Meal Description:</label>
+        <input [(ngModel)]="selectedMeal.description">
+      </div>
+      <div>
+        <label>Enter Calories:</label>
+        <input [(ngModel)]="selectedMeal.calories">
+        <button (click)="finishedEditing()">Done</button>
+      </div>
     </div>
   </div>
   `
@@ -25,15 +31,17 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   public meals: Meal[] = [
-  new Meal("Tasty Tacos", "delicous tacos", 354),
-  new Meal("Meat Pie", "delicous tacos", 354),
-  new Meal("Fish Burritos", "delicous tacos", 354),
-  new Meal("Cat Tongue", "delicous tacos", 354),
+    new Meal("Tasty Tacos", "delicous tacos", 354),
+    new Meal("Meat Pie", "delicous tacos", 354),
+    new Meal("Fish Burritos", "delicous tacos", 354),
+    new Meal("Cat Tongue", "delicous tacos", 354),
   ];
-  selectedMeal: Meal = this.meals[0];
+  selectedMeal: Meal = null;
   showDetails(clickedMeal: Meal) {
     this.selectedMeal = clickedMeal;
-
+  }
+  finishedEditing() {
+    this.selectedMeal = null;
   }
 }
 
